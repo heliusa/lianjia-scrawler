7534a038a99d8c0660b9e9488fec9933e8802c8f
heliusa <makeapple123@gmail.com>
2019-10-29 23:54:38 +0800

    setting

--- settings.py
+++ settings.py
@@ -1,8 +1,15 @@
+#coding:utf-8
 DBENGINE = 'mysql'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
-DBNAME = 'ershoufang'
+DBNAME = 'lianjia'
 DBUSER = 'root'
-DBPASSWORD = ''
+DBPASSWORD = 'a123456'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
-CITY = 'bj'  # only one, shanghai=sh shenzhen=sh......
-REGIONLIST = [u'dongcheng']  # only pinyin support
+CITY = 'wh'  # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'dongxihu', 'jiangan']  # only pinyin support
+
+
+CHANNEL_PARAM = {
+    'lianjia' : {'CITY': 'wh', 'REGIONLIST' : [u'dongxihu', u'jiangan'] },
+    'fang': {'CITY': 'wuhan', 'REGIONLIST' :[u'东西湖', u'江岸']  }
+}
